90b7f4325ea37389a6b7dd047f25c08e19ed47f9
邵励治 <shaolizhi1234@126.com>
2018-10-21 16:55:54 +0800

    成功的下载了TEAM数据：球队名、小组赛分组

--- seleniumDemo.py
+++ seleniumDemo.py
@@ -6,13 +6,8 @@ try:
     # 寻找 标签名：table name=A中的内容
     table = browser.find_elements_by_tag_name("table")
     for item in table:
-        print(item.get_attribute("groupname"))
-
-    # urls = browser.find_elements_by_xpath("//a")
-    # for url in urls:
-    #     print(url.text)
-    # a = browser.find_element_by_link_text("http://2018.163.com/speecial/team4725")
-    # print(a.text)
+        for a in item.find_elements_by_tag_name("a"):
+            print(item.get_attribute("groupname") + " " + a.text)
 except Exception as exception:
     print(exception)
-    print("Was Not Able To Find An Element With That Name.")
+print("Was Not Able To Find An Element With That Name.")
